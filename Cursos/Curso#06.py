8c88edc1803d148ce08090adc2b7d1919750ca33
Sarah781 <sarah7bvs@gmail.com>
2022-12-21 09:03:24 -0300

    aulas 5 e 6

---
+++ Cursos/Curso#06.py
@@ -0,0 +1,21 @@
+#int=números inteiros (4,7,-4,95)
+#float=números com pontos flutuantes(4.5,7.0,0.07)
+#bool=números verdadeiros ou falsos(True, False)
+#str=escrita("Olá", 'tudo bem?')
+
+
+
+
+#Desafio03
+#n1=int(input('Primeiro Número: '))
+#n2=int(input('Segundo Número: '))
+#s=n1+n2
+#print('a soma entre {} + {} vale {} '.format(n1,n2,s))
+
+n=input('Digite algo: ')
+print(n.isdecimal())
+
+#isalnum= verifica se é um numero
+#isalpha= verifica se é uma letra do alfabeto
+#isspace= verifica se tem espaço
+#isdecimal= verifica se é um numero decimal
