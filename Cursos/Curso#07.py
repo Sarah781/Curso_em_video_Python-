1afeed1271fe5203fc268f7d9fd8bf4a227ec39d
Sarah781 <sarah7bvs@gmail.com>
2022-12-23 10:59:18 -0300

    exercios 7 e 8

--- Cursos/Curso#07.py
+++ Cursos/Curso#07.py
@@ -35,34 +35,50 @@
 
 #Desafio 05 Mostre o número inteiro, seu antecessor e sucessor
 #n=int(input('Digite um Número:'))
-#print ('{},{},{}'.format(n-1, n, n+1))
+#print ('O antecessor do numero {} é {} e seu sucessor é {}'.format(n, n-1, n+1))
 
 #______________________________________________________________________________________________
 
 #Desafio 06 leia um número e mostre o seu dobro, triplo e raiz quadrada
 #n=int(input('Digite um número:'))
-#print('O dobro do número {}, é {}, o triplo é {}, e a sua raiz quadrada é {}'.format(n,n*2,n*3,n**2))
+#print('O dobro do número {}, é {}. \nO triplo é {}. \nE a sua raiz quadrada é {:.2f}'.format(n,n*2,n*3,n**(1/2)))
 
 #______________________________________________________________________________________________
 
 #Desafio 07 leia as duas notas de um aluno, calcule e mostre a sua média 
-#n1=float(input('Digite a nota com .'))
-#n2=float(input('Insira a segunda nota com .'))
+#n1=float(input('Digite a primeira nota:'))
+#n2=float(input('Insira a segunda nota:'))
 #media=(n1+n2)/2
 #print('A média final é {}'.format(media))
 
 #______________________________________________________________________________________________
 
 #Desafio 08 leia um valor em metros e exiba convertido em centímetros a milímetros
+#n=float(input('Insira um número:'))
+#cent=(n*100)
+#mil=(n*1000)
+#print('O valor {}, em centímetros é {:.2f}, e em milímetros é {:.2f}'.format(n,cent,mil))
 
 #______________________________________________________________________________________________
 
 #Desafio 09 leia um número qualquer e mostre na tela a sua tabuada
-
+#n=int(input('Insira um número para tabuada:'))
+#print('{} X {:2} = {}'.format(n,1,n*1))
+#print('{} X {:2} = {}'.format(n,2,n*2))
+#print('{} X {:2} = {}'.format(n,3,n*3))
+#print('{} X {:2} = {}'.format(n,4,n*4))
+#print('{} X {:2} = {}'.format(n,5,n*5))
+#print('{} X {:2} = {}'.format(n,6,n*6))
+#print('{} X {:2} = {}'.format(n,7,n*7))
+#print('{} X {:2} = {}'.format(n,8,n*8))
+#print('{} X {:2} = {}'.format(n,9,n*9))
+#print('{} X {:2} = {}'.format(n,10,n*10))
 #______________________________________________________________________________________________
 
 #Desafio 10 leia quanto dinheiro ela tem na carteira e mostre quantos dólares ela pode comprar Considere 1 dólarR$3,27
-
+din=float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar=din/3.27
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(din,dolar))
 #______________________________________________________________________________________________
 
 #Desafio 11 leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la sabendo que cada litro de tintapinta uma área de 2m2
---
+++ Cursos/Curso#08.py
@@ -0,0 +1,26 @@
+#bibliotecas
+#import math 
+#from math import sqrt (para apenas selecionar uma das funcionalidades)
+#ceil arredonda para cima 
+# floor arredonda para baixo
+#trunc tira os números depois da vírgula
+#pow faz a função dos **
+#sqrt calculo de raiz quadrada
+#factorial cálculo de fatorial de um número
+
+import emoji
+  
+print(emoji.emojize(":grinning_face_with_big_eyes:"))
+
+
+#Desafio 16 leia um numero raal qualquer pelo teclado e mostre a sua porção inteira ex:6127 sua porção inteira é 6
+
+#Desafio 17 leia o cumprimento de cateto oposto e do catedo adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+
+#Desafio 18 leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo
+
+#Desafio 19 um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido sort
+
+#Desafio 20 o mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalho dos alunos.Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+
+#Desafio 21 faça um programa em python que abra e reproduza o áudio de um arquivo MP3
