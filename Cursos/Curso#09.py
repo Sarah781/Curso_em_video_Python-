c521bda076072a575a566ab1e48b7a9b5d1eb55f
Sarah781 <sarah7bvs@gmail.com>
2022-12-26 09:27:31 -0300

    curso9

--- Cursos/Curso#09.py
+++ Cursos/Curso#09.py
@@ -37,7 +37,6 @@
 #quantas letras tem o primeiro nome
 
 
-
 #______________________________________________________________________________________________
 
 #Desafio23 Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados 
