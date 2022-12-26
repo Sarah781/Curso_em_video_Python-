3aa348d6250f360ef78da5109e48b90536288a47
Sarah781 <sarah7bvs@gmail.com>
2022-12-26 09:26:27 -0300

    curso9

---
+++ Cursos/Curso#09.py
@@ -0,0 +1,75 @@
+#frase='Curso em Video Python'#A primeira letra começa do número 0
+#frase[9:14] #vai do 9 ao 14
+#frase[9:21:2] #vai do 9 ao 21 pulando de 2 em 2
+#frase[:5] #vai do inicio até o 5
+#frase[15:] #vai do 15 até o final
+#frase[9::3]#vai começar do 9 e vai ate o final, porem ele pula de 3 em 3 
+#len(frase) =comprimento da frase
+#frase.count('o') #contar quantas vezes aparece o 'o'
+#frase.find('deo') #indica a posição que se encontra
+#'Curso' in frase #pergunta se existe 'Curso' na frase
+#frase.replace('Python','Android') #aonde estiver escrito python ele vai substituir por android
+#frase.upper() #ele pega todas as letras minúsculas e transforma em maiúsculas
+#frase.lower() #transforma todas as letras em minúsculas
+#frase.capitalize #ele pega todas as letras coloca em minúsculas e somente a primeira em maiúscula
+#frase.title() #vai analisar quantas palavras existem
+#frase.strip() #vai remover todos os espaços inúteis
+#frase.rstrip() #ele vai remover os espaços da direira
+#frase.lstrip() #ele vai remover os espaços da esquerda
+#frase.split() #ele vai pegar aonde tem espaços e fazer uma divisão ou seja ele pega as palavras separa e cria uma outra lista
+#'-'.join(frase) #ele vai juntar todas as palavras e vai separar com -
+
+#frase='Curso em Video Python'
+#print(frase[1:15:2])
+
+#frase='Curso em Video Python'
+#print(frase.count('o'))
+
+#frase='Curso em Video Python'
+#print('Curso' in frase)
+
+#______________________________________________________________________________________________
+
+#Desafio22 Crie um programa que leia o nome completo de uma pessoa e mostre:
+#o nome com todas as letras maiúsculas
+#o nome com todas as letra minúsculas
+#quantas letras ao todo(sem considerar espaços)
+#quantas letras tem o primeiro nome
+
+
+
+#______________________________________________________________________________________________
+
+#Desafio23 Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados 
+#ex: entrada: digite um número:1834
+#saida: unidade:4
+#       dezena:3
+#       centena:8
+#       milhar:1
+
+
+#______________________________________________________________________________________________
+
+#Desafio24 Crie  um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'SANTO'
+
+#______________________________________________________________________________________________
+
+#Desafio25 Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome
+
+#______________________________________________________________________________________________
+
+#Desafio26 Faça um programa que leia uma frase pelo teclado e mostre:
+#quantas vezes aparece a letra 'A'
+#em que posição ela aparece pela primeira vez
+#em que posição aparece pela última vez
+
+#______________________________________________________________________________________________
+
+#Desafio27 Faça um programa que leia o nome completo de um pessoa, 
+#mostrando em seguida o primeiro e o último nome separadamente.
+#entrada:ana maria de souza
+#saida: primeiro=ana
+#       último=souza
+
+
+
