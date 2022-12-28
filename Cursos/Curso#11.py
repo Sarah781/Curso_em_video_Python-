7dbb302f3d2acadb30b9a8bf8497767d739f3f31
Sarah781 <sarah7bvs@gmail.com>
2022-12-28 08:38:38 -0300

    cores no terminal

---
+++ Cursos/Curso#11.py
@@ -0,0 +1,7 @@
+#para colocar cor no terminal \033[0;33;44m - dentro do colchete 0 = style, 33= text, 44= background
+#style= 0,1,4,7 = 0 sem estilo,1= negrito, 4= sublinhado, 7 inversão de cores ou negativo
+#text= 30,31,32,33,34,35,36,37 = 30 branco, 31 vermelho, 32 verde, 33 amarelo, 34 azul, 35 roxo, 36 verde agua, 37 cinza 
+#background =40,41,42,43,44,45,46,47 = 40 branco, 41 vermelho, 42 verde, 43 amarelo, 44 azul, 45 roxo, 46 verde agua, 47 cinza
+
+#nome='Sarah'
+#print('Olá muito prazer em te conhecer {}{}{}!'.format('\033[4;34m',nome,'\033[m'))
